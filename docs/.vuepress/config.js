7c7410a59f63a1e40a1b167d9ee53fc4b86ffaaf
帅哥俊呀 <shuaigejun@163.com>
2020-06-29 00:21:00 +0800

    chrome(初始哈):blog初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    base:'/clog/',
+    title: 'Zhong \'s blog',
+    description: 'Just playing around',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+}
