7eb187c1f8caac3e44472d70d11d4148f713a240
帅哥俊呀 <shuaigejun@163.com>
2020-07-17 11:41:44 +0800

    add:完成第一篇个人博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,6 +37,24 @@ module.exports = {
         nav: [
             // 导航栏配置
             { text: '首页', link: '/' }
-        ]
+        ],
+        footer: {
+            contact: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/phoenix355'
+                }
+            ],
+            copyright: [
+                {
+                  text: 'Privacy Policy',
+                  link: 'https://policies.google.com/privacy?hl=en-US',
+                },
+                {
+                  text: 'MIT Licensed | Copyright © 2020-present 版权归帅哥俊所有',
+                  link: '',
+                },
+              ],
+        }
     }
 }
